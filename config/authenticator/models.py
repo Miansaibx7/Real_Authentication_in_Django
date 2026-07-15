86164906897efc58c618f91a670a9dcc8a981731
MiansaibX7 <mianmwaqas77@gmail.com>
2026-07-15 08:12:52 +0500

    Initial commit

--- config/authenticator/models.py
+++ config/authenticator/models.py
@@ -47,7 +47,7 @@ class EmailOTP(models.Model):
     attempts = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
 
-    def is_expired(self):
+    def is_expired(self)-> None:
         return timezone.now() > self.created_at + timedelta(minutes=10)
 
     def __str__(self):
