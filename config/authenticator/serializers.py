93d0d4411fe658efe6c3f745a6f26ce74777c22a
MiansaibX7 <mianmwaqas77@gmail.com>
2026-07-16 23:17:31 +0500

    Initial commit

--- config/authenticator/serializers.py
+++ config/authenticator/serializers.py
@@ -6,7 +6,7 @@ from rest_framework.validators import UniqueValidator
 User = get_user_model()
 
 class RegisterSerializer(serializers.ModelSerializer):
-# Serializers for New User Registration 
+# Serializers for User Registration 
     email = serializers.EmailField(required=True,validators=[UniqueValidator(queryset=User.objects.all(),
                                          message="User with this email already exists.")])
     password = serializers.CharField(write_only=True, required=True, validators=[validate_password], trim_whitespace=False)
