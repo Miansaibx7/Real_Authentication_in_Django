22b7f96df7345fbc534829958c5ee77ed8b8ed0e
MiansaibX7 <mianmwaqas77@gmail.com>
2026-07-17 09:34:50 +0500

    Initial commit

--- config/authenticator/serializers.py
+++ config/authenticator/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from django.contrib.auth import authenticate, get_user_model
+from django.contrib.auth import get_user_model
 from django.contrib.auth.password_validation import validate_password
 from rest_framework.validators import UniqueValidator
 
