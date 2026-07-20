c007b1e752939dc94f150d463aa0e1ed45a71436
MiansaibX7 <mianmwaqas77@gmail.com>
2026-07-20 20:18:52 +0500

    Initial commit

--- config/authenticator/urls.py
+++ config/authenticator/urls.py
@@ -5,9 +5,13 @@ from rest_framework_simplejwt.views import (TokenRefreshView)
 urlpatterns = [
     # url for register
     path("register/", RegisterView.as_view(), name="register"),
+    # url for VerifyOTP
     path("verify-otp/", VerifyOTPView.as_view(), name="verify-otp"),
+    # url for Login
     path("login/", LoginView.as_view(), name="login"),
+    # url for ForgotPassword
     path("forgot-password/", ForgotPasswordView.as_view(), name="forgot-password"),
+    
     path("reset-password/", ResetPasswordView.as_view(), name="reset-password"),
 
 # refresh token endpoint for JWT authentication
