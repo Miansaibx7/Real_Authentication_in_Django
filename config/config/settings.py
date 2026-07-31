e09bb56477ef90b3bdec13144d38c70256a1bb73
MiansaibX7 <mianmwaqas77@gmail.com>
2026-07-31 11:17:25 +0500

    Initial commit

--- config/config/settings.py
+++ config/config/settings.py
@@ -91,7 +91,6 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
