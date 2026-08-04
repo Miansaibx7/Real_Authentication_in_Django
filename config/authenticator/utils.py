b3fcb6530e64418b78174af1816cd8daff4339d4
MiansaibX7 <mianmwaqas77@gmail.com>
2026-08-04 23:52:13 +0500

    Initial commit

--- config/authenticator/utils.py
+++ config/authenticator/utils.py
@@ -13,7 +13,7 @@ def send_email_otp(email, code):
     try:
         send_mail(subject="Your Verification Code", message=f"Your OTP code is: {code}",
             from_email=settings.EMAIL_HOST_USER, recipient_list=[email],
-            fail_silently=False,
+            fail_silently=False
         )
     except Exception as e:
         print(f"SMTP Setup Error: {e}") # This prevents the registration endpoint from crashing 500 if email fails
