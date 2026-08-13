c5d2953e48fe5a014538b5b0c399829cb614bf17
MiansaibX7 <mianmwaqas77@gmail.com>
2026-08-13 10:17:09 +0500

    Initial commit

--- config/authenticator/serializers.py
+++ config/authenticator/serializers.py
@@ -8,7 +8,7 @@ User = get_user_model()
 class RegisterSerializer(serializers.ModelSerializer):
 # Serializers for User Registration 
     email = serializers.EmailField(required=True,validators=[UniqueValidator(queryset=User.objects.all(),
-                                         message="User with this email already exists.")])
+            message="User with this email already exists.")])
     password = serializers.CharField(write_only=True, required=True, validators=[validate_password], trim_whitespace=False)
     confirm_password = serializers.CharField(write_only=True, required=True)
 
