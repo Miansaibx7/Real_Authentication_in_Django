796423c4cb127f1efa64107a8b1f2a6a97003cff
MiansaibX7 <mianmwaqas77@gmail.com>
2026-08-13 10:14:11 +0500

    Initial commit

--- config/authenticator/urls.py
+++ config/authenticator/urls.py
@@ -7,12 +7,12 @@ urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
     # Endpoint  for VerifyOTP
     path("verify-otp/", VerifyOTPView.as_view(), name="verify-otp"),
-    # endpoint for Login
+    # Endpoint for Login
     path("login/", LoginView.as_view(), name="login"),
     # Endpoint for ForgotPassword
     path("forgot-password/", ForgotPasswordView.as_view(), name="forgot-password"),
     # Endpoint for ResetPassword
     path("reset-password/", ResetPasswordView.as_view(), name="reset-password"),
-    # refresh token endpoint for JWT authentication
+    # Refresh token endpoint for JWT authentication
     path('token/refresh/', TokenRefreshView.as_view()),
 ]
