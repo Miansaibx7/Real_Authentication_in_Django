80f7c5702c71cd57d9c1af59597b7a8b8acda9fb
MiansaibX7 <mianmwaqas77@gmail.com>
2026-08-13 10:14:45 +0500

    Initial commit

--- config/authenticator/urls.py
+++ config/authenticator/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import (RegisterView, VerifyOTPView,LoginView,ForgotPasswordView,ResetPasswordView,)
-from rest_framework_simplejwt.views import (TokenRefreshView)
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     # Endpoint for register
